c5da951c891ce067a9f52fdf60f37ac86f5b36f0
Andrew Chow <achow101-github@achow101.com>
2018-10-31 19:02:36 -0400

    unpack header

--- week/8/stub.py
+++ week/8/stub.py
@@ -3,6 +3,7 @@
 import sys
 import struct
 import argparse
+import datetime
 
 parser = argparse.ArgumentParser(description='Parses FPFF files')
 parser.add_argument('file', help='Filename of FPFF file to parse')
@@ -22,10 +23,8 @@ VERSION = 1
 with open(args.file, 'rb') as fpff:
     data = fpff.read()
 
-# Hint: struct.unpack will be VERY useful.
-# Hint: you might find it easier to use an index/offset variable than
-# hardcoding ranges like 0:8
-magic, version = struct.unpack("<LL", data[0:8])
+# Unpack header
+magic, version, timestamp, author, section_count = struct.unpack("<LLL8sL", data[0:24])
 
 if magic != MAGIC:
     bork("Bad magic! Got {}, expected {}".format(hex(magic), hex(MAGIC)))
@@ -36,6 +35,9 @@ if version != VERSION:
 print("------- HEADER -------")
 print("MAGIC: {}".format(hex(magic)))
 print("VERSION: {}".format(int(version)))
+print("TIMESTAMP: {} ({} UNIX Timestamp)".format(datetime.datetime.utcfromtimestamp(timestamp), timestamp))
+print("AUTHOR: {}".format(author.decode()))
+print("SECTION COUNT: {}".format(section_count))
 
 # We've parsed the magic and version out for you, but you're responsible for
 # the rest of the header and the actual FPFF body. Good luck!
