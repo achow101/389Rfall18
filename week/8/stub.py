d19d0099f096b97311904a4ad8987fe028812dd5
Andrew Chow <achow101-github@achow101.com>
2018-10-31 18:57:19 -0400

    Your python is bad and you should feel bad
    I'm getting cancer from your shit python

--- week/8/stub.py
+++ week/8/stub.py
@@ -1,8 +1,12 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import sys
 import struct
+import argparse
 
+parser = argparse.ArgumentParser(description='Parses FPFF files')
+parser.add_argument('file', help='Filename of FPFF file to parse')
+args = parser.parse_args()
 
 # You can use this method to exit on failure conditions.
 def bork(msg):
@@ -13,12 +17,9 @@ def bork(msg):
 MAGIC = 0xdeadbeef
 VERSION = 1
 
-if len(sys.argv) < 2:
-    sys.exit("Usage: python2 stub.py input_file.fpff")
-
 # Normally we'd parse a stream to save memory, but the FPFF files in this
 # assignment are relatively small.
-with open(sys.argv[1], 'rb') as fpff:
+with open(args.file, 'rb') as fpff:
     data = fpff.read()
 
 # Hint: struct.unpack will be VERY useful.
@@ -27,14 +28,14 @@ with open(sys.argv[1], 'rb') as fpff:
 magic, version = struct.unpack("<LL", data[0:8])
 
 if magic != MAGIC:
-    bork("Bad magic! Got %s, expected %s" % (hex(magic), hex(MAGIC)))
+    bork("Bad magic! Got {}, expected {}".format(hex(magic), hex(MAGIC)))
 
 if version != VERSION:
-    bork("Bad version! Got %d, expected %d" % (int(version), int(VERSION)))
+    bork("Bad version! Got {}, expected {}".format(int(version), int(VERSION)))
 
 print("------- HEADER -------")
-print("MAGIC: %s" % hex(magic))
-print("VERSION: %d" % int(version))
+print("MAGIC: {}".format(hex(magic)))
+print("VERSION: {}".format(int(version)))
 
 # We've parsed the magic and version out for you, but you're responsible for
 # the rest of the header and the actual FPFF body. Good luck!
